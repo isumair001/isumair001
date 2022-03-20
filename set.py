07c652ab968d31d5a9d70db659e63f802a529d81
isumair001 <101408442+isumair001@users.noreply.github.com>
2022-03-20 17:39:33 +0500

    Create set.py

---
+++ set.py
@@ -0,0 +1,5 @@
+#program a set having 18 as integer and string value.
+s = {18, "18"}
+print(s)
+print(type(s))
+#{'18', 18} first value 18 is string, second one is integer. both are unique for set. so both are printed in set.
