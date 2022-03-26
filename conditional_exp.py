865004e382604f2c0b1475efb317e66c60f3cae0
isumair001 <101408442+isumair001@users.noreply.github.com>
2022-03-26 12:24:31 +0500

    Create conditional_exp.py

---
+++ conditional_exp.py
@@ -0,0 +1,11 @@
+#if , elif, and else conditional expressions.
+a = 45
+if (a>46):
+    print("The value of a is greater than 46")
+elif(a>13):
+    print("The value of a is greater than 13")
+elif(a>7):
+    print("The value of a is greater than 7")
+else:
+    print("The value of a is not greater than 9 or 13")
+    ''' the result will be first true statement among the given statements of if - else ladder. so, the ladder will stop once a condition mets to statement.'''
