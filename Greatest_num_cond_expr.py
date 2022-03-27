d8c95c4f4eb06f96ff5d9cb6dfd3b17d5ce952d1
isumair001 <101408442+isumair001@users.noreply.github.com>
2022-03-27 11:28:01 +0500

    Create Greatest_num_cond_expr.py

---
+++ Greatest_num_cond_expr.py
@@ -0,0 +1,24 @@
+#ALT + click for multi cursors in vscode editor
+
+#Find the greatest of four numbers
+
+num1 = int(input("Enter number 1: "))
+num2 = int(input("Enter number 2: "))
+num3 = int(input("Enter number 3: "))
+num4 = int(input("Enter number 4: "))
+
+"using conditional exprressions"
+if (num1>num4):
+    f1 = num1
+else:
+    f1 = num4
+
+if(num2>num3):
+    f2 = num2
+else:
+    f2 = num2
+
+if (f1>f2):
+    print(str(f1) + "is the greatest number")
+else:
+    print(str(f2) + "is the greatest number")
