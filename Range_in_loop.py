555ea2b941cc17050e2c4c7d83c55da35c256586
isumair001 <101408442+isumair001@users.noreply.github.com>
2022-04-06 15:52:39 +0500

    Create Range_in_loop.py

---
+++ Range_in_loop.py
@@ -0,0 +1,3 @@
+# range is used in python to generate a sequence of numbers. we need to specify start, stop and step size in range. usually step size is not used in range. it goes upto n-1.
+for i in range (0,18, 3):
+    print(i)
