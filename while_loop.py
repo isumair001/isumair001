5cb2daa4ad521fdd5f258eb9036085b54cc9dd92
isumair001 <101408442+isumair001@users.noreply.github.com>
2022-04-06 15:22:06 +0500

    Create while_loop.py

---
+++ while_loop.py
@@ -0,0 +1,11 @@
+i = 4
+while i <= 10:  #While loop block keeps rolling until it proves to be true or false.
+    print("yes " + str(i))
+    i = i + 1
+    ''' condition is the primary element,
+    that is checked first in loop,
+    either it is true or false.
+    if true it will go ahead to execute program,
+    otherwise it wont.
+    More often thae value of "i" is desired to change to make condition false at certain levels in order to stop the execution of loop. '''
+print("done")
