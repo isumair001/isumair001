9168910d5184a4b91c3b5bfe9dcb58ed91521d51
isumair001 <101408442+isumair001@users.noreply.github.com>
2022-04-07 16:45:40 +0500

    Create Factorial.py

---
+++ Factorial.py
@@ -0,0 +1,7 @@
+# Formula for factorial is n! = 1 X 2 X 3 X ..... X n
+
+num = int(input("Enter the number: "))
+factorial = 1
+for i in range(1, num+1):
+    factorial = factorial * i
+print(f"The factorial of {num} is {factorial}")
