e19618d6210b80688d996fd2a85362d835a20bb0
isumair001 <101408442+isumair001@users.noreply.github.com>
2022-04-16 12:35:28 +0500

    Create find factorial using recursive.py

---
+++ find factorial using recursive.py
@@ -0,0 +1,13 @@
+from math import factorial
+from re import I
+
+
+#factorial n = n * factorial(n-1)
+def factorial_recursive(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n*factorial_recursive(n-1)
+# f = factorial_iter(5)
+f = factorial_recursive(5)
+print(f)
