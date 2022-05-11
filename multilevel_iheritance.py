177af18c5dde8bee81e276901ff4b8c56ef5eef8
isumair001 <101408442+isumair001@users.noreply.github.com>
2022-05-11 10:53:33 +0500

    Create multilevel_iheritance.py

---
+++ multilevel_iheritance.py
@@ -0,0 +1,35 @@
+class Person:
+    country = "Pakistan"
+    def takeBreath(self):
+        print("I am breathing...")
+
+class Employee(Person):
+    company = "Microsoft"
+
+    def getSalary(self):
+        print(f"Salary is {self.salary}")
+    
+    def takeBreath(self):
+        print("I am an Employee so I am luckily breathing..")
+
+class Programmer(Employee):
+    company = "Upwork"
+    
+    def getSalary(self):
+        print(f"No salary to programmers")
+    
+    def takeBreath(self):
+        print("I am a Progarmmer so I am breathing++..")
+
+p = Person()
+p.takeBreath()
+# print(p.company) # throws an error
+
+e = Employee()
+e.takeBreath()
+print(e.company)
+
+pr = Programmer()
+pr.takeBreath()
+print(pr.company)
+print(pr.country)
